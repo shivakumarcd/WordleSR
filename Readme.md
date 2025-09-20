7988a040f1989da4a7a67faf9a1ac3f3e5335d88
Shivakumar Doddamani <mailtoshivoo@gmail.com>
2025-09-20 17:44:40 +0200

    Create sample gradle project

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## Requirements
+- Java 20 (JDK 20) to run this application
+
+## Build & Test
+```bash
+./gradlew build
+./gradlew test
+```
---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(20))
+    }
+}
+
+group = "com.match"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
